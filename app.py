Fixed scaling up to numpy int16

app.py
@@ -36,7 +36,11 @@ def synthesise(text):
 def speech_to_speech_translation(audio):
     translated_text = translate(audio)
     synthesised_speech = synthesise(translated_text)
-    synthesised_speech = (synthesised_speech.numpy() * 32767).astype(np.int16)
+    
+    # normalise the speech signal
+    target_dtype = np.int16
+    max_range = np.iinfo(target_dtype).max
+    synthesised_speech = (synthesised_speech.numpy() * max_range).astype(np.int16)
     return 16000, synthesised_speech
 
 